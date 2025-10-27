4e1ce98cf2d76538d90d116ed0af3426076de315
Gabriel Santos Rezende <gabrielthel9@gmail.com>
2025-10-27 15:03:01 -0300

    refactor: remove console logs and improve scroll functionality

--- js/script.js
+++ js/script.js
@@ -1,40 +1,47 @@
 window.onload = () => {
+    const cSobre = Math.floor($('#conteudoSobre').offset().top);
+    const cExp = Math.floor($('#conteudoExperiencia').offset().top);
+    const cEdu = Math.floor($('#conteudoEducacao').offset().top);
+    
 
     $('#menu ul a').each(function () {
         let btnID = $(this).attr('id');
-        console.log(this);
-        console.log($(this).attr('id'));
 
-        console.log($('#conteudoSobre').offset().top);
-        console.log($('#conteudoExperiencia').offset().top);
-        console.log($('#conteudoEducacao').offset().top);
         $(this).click(function (e) {
-            
             e.preventDefault();
             //scroll-click
             if (btnID == 'btnSobre') {
-                console.log($('#conteudoSobre').offset().top);
                 $('#content').stop().animate({
                     scrollTop: $('#conteudoSobre').offset().top - $('#wrapper').offset().top
                 }, 600);
             }else if (btnID == 'btnExperiencia') {
-                console.log($('#conteudoExperiencia').offset().top);
                 $('#content').stop().animate({
                     scrollTop: $('#conteudoExperiencia').offset().top - $('#wrapper').offset().top
                 }, 600);
             }else if (btnID == 'btnEducacao')  {
-                console.log($('#conteudoEducacao').offset().top);
                 $('#content').stop().animate({
                     scrollTop: $('#conteudoEducacao').offset().top - $('#wrapper').offset().top
                 }, 600);
             }
-
-            //reset last link clicked with color
-            $('ul a').each(function() {
-                $(this).css('color', 'rgb(83, 115, 127)');
-            });
-            $(this).css('color', 'azure'); 
         });
     });
-
+    //scroll-mouse
+    $('#content').on('scroll', function() {
+        if($('#conteudoSobre').offset().top > ((cExp * -1) + (cExp / 3))) {
+            $('#menu ul a').each(function () {
+                $(this).css('color', 'rgb(83, 115, 127)');
+            });
+            $('#menu ul #btnSobre').css('color', 'azure');
+        }else if(($('#conteudoSobre').offset().top + (cExp / 3)) > ((cEdu * -1) + (cEdu / 3))) {
+            $('#menu ul a').each(function () {
+                $(this).css('color', 'rgb(83, 115, 127)');
+            });
+            $('#menu ul #btnExperiencia').css('color', 'azure'); 
+        }else {
+            $('#menu ul a').each(function () {
+                $(this).css('color', 'rgb(83, 115, 127)');
+            });
+            $('#menu ul #btnEducacao').css('color', 'azure'); 
+        }
+    });
 }
