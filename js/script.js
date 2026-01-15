dfa08e5f35f92076382279bd5300a8763dcd3caf
Gabriel Santos Rezende <gabrielthel9@gmail.com>
2026-01-15 12:41:38 -0300

    feat: implement smooth scrolling and update navigation links for sections

--- js/script.js
+++ js/script.js
@@ -3,28 +3,6 @@ window.onload = () => {
     const cExp = Math.floor($('#conteudoExperiencia').offset().top);
     const cEdu = Math.floor($('#conteudoEducacao').offset().top);
     
-
-    $('#menu ul a').each(function () {
-        let btnID = $(this).attr('id');
-
-        $(this).click(function (e) {
-            e.preventDefault();
-            //scroll-click
-            if (btnID == 'btnSobre') {
-                $('#content').stop().animate({
-                    scrollTop: $('#conteudoSobre').offset().top - $('#wrapper').offset().top
-                }, 600);
-            }else if (btnID == 'btnExperiencia') {
-                $('#content').stop().animate({
-                    scrollTop: $('#conteudoExperiencia').offset().top - $('#wrapper').offset().top
-                }, 600);
-            }else if (btnID == 'btnEducacao')  {
-                $('#content').stop().animate({
-                    scrollTop: $('#conteudoEducacao').offset().top - $('#wrapper').offset().top
-                }, 600);
-            }
-        });
-    });
     //scroll-mouse
     $('#content').on('scroll', function() {
         if($('#conteudoSobre').offset().top > ((cExp * -1) + (cExp / 3))) {
